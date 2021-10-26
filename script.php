be7870b56b9995611b0a1e3bbbf7406b1a5e6b2b
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-10-26 19:57:42 -0300

    Update script.php

--- script.php
+++ script.php
@@ -13,6 +13,37 @@
 @$nome = $_POST['nome'];
 @$idade = $_POST['idade'];
 
+// Validando dados do formulário
+if(empty($nome)) // O campo não pode ficar vazio
+{
+    echo 'O nome não pode ser vazio!';
+    return;
+}
+
+if(strlen($nome) < 3) // Restringe quantidade minima de caracteres
+{
+    echo 'O nome não pode conter menos de três caracteres';
+    return;
+}
+
+if(strlen($nome) > 40) // Restringe a quantidade máxima de caracteres
+{
+    echo 'O nome é muito extenso!';
+    return;
+}
+
+if(!is_numeric($idade)) // Permite apenas números para o campo idade
+{
+    echo 'Informe um número correto para idedade!';
+    return;
+}
+
+if(strlen($idade) > 3) // Não aceita número acima de três digitos
+{
+    echo 'O campo idade não pode ser superior a três números!';
+    return;
+}
+
 //var_dump($nome);
 //var_dump($idade);
 
