49979ffeba9795bb3e057892e52a92ddd655c6a3
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-10-26 22:50:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,23 +9,31 @@ session_start(); // Iniciando a sessão
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Incrição para Competidores</title>
+    <link rel="stylesheet" href="servicos/style.css">
 </head>
 <body>
 
-    <p>Formulário para Inscrição de Competidores</p>
+    <div id="formulario">
 
-    <form action="script.php" method="post">
-        <?php // Retona mensagem de erro
-        $mensagemDeErro = isset($_SESSION['mensagem de erro']) ? $_SESSION['mensagem de erro'] : '';
-        if(!empty($mensagemDeErro))
-        {
-            echo $mensagemDeErro;
-        }
-        ?>
-        <p>Seu nome: <input type="text" name="nome" placeholder="Digite seu nome" require></p>
-        <p>Sua idade: <input type="text" name="idade" placeholder="Sua idade" require></p>
-        <p><input type="submit" value="Enviar dados do competidor" /><p>       
-    </form>
+        <p>Formulário para Inscrição de Competidores</p>
+
+        <form action="script.php" method="post">
+
+            <h1><?php // Retona mensagem informando o status do cadastro
+            $mensagemDeRetorno = isset($_SESSION['mensagem']) ? $_SESSION['mensagem'] : '';
+            if(!empty($mensagemDeRetorno))
+            {
+                echo $mensagemDeRetorno;
+            }
+            ?></h1>
+
+            <p>Seu nome: <input type="text" name="nome" placeholder="Digite seu nome" require></p>
+            <p>Sua idade: <input type="text" name="idade" placeholder="Sua idade" require></p>
+            <p><input type="submit" value="Enviar dados do competidor" /><p>   
+
+        </form>
+
+    </div>
     
 </body>
 </html>
